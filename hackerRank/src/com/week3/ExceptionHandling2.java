9a85ec578461a9e1dd7c62759e5913c43eeea6a9
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-11-23 00:38:32 +0530

    Exception Handling

---
+++ hackerRank/src/com/week3/ExceptionHandling2.java
@@ -0,0 +1,40 @@
+package com.week3;
+import java.util.Scanner;
+class MyCalculator {
+    /*
+    * Create the method long power(int, int) here.
+    */
+	public long power(int n, int p) throws Exception{
+	    long result;
+	    if(n==0 && p==0){
+	        throw new Exception("n and p should not be zero.");
+	    }
+	    if(n<0 || p<0){
+	        throw new Exception("n or p should not be negative.");
+	    }
+	         result=(long)Math.pow(n,p);
+	         return result;
+    }
+}
+
+
+public class ExceptionHandling2 {
+	public static final MyCalculator my_calculator = new MyCalculator();
+    public static final Scanner in = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        while (in .hasNextInt()) {
+            int n = in .nextInt();
+            int p = in .nextInt();
+            
+            try {
+                System.out.println(my_calculator.power(n, p));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+
+ 
