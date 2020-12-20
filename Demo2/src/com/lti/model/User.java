6a0f9692433ca69f73a1fd09effb60eb76cc54f2
Ashleshk <ashleshuk@gmail.com>
2020-12-20 16:20:08 +0530

    User java

---
+++ Demo2/src/com/lti/model/User.java
@@ -0,0 +1,24 @@
+package com.lti.model;
+
+public class User {
+	public void method(){
+		System.out.println("xc");
+	}
+	
+	public void method2(){
+		System.out.println("method 2");
+	}
+	public void method3(){
+		System.out.println("method 3");
+	}
+	
+	public void method4(){
+		System.out.println("method 4");
+	}
+	public static void main(String[] args) {
+		
+		int x =1;
+		int  y=101;
+		System.out.println("ashlesh khajbage");
+	}
+}
