843154f845692797fc5eb14b6ba22402a18f9f95
Ashleshk <ashleshuk@gmail.com>
2020-12-20 16:19:59 +0530

    Persons Java

---
+++ Demo2/src/com/lti/model/Persons.java
@@ -0,0 +1,26 @@
+package com.lti.model;
+
+public class Persons {
+	int personId;
+	String personName;
+	String personEmail;
+	public int getPersonId() {
+		return personId;
+	}
+	public void setPersonId(int personId) {
+		this.personId = personId;
+	}
+	public String getPersonName() {
+		return personName;
+	}
+	public void setPersonName(String personName) {
+		this.personName = personName;
+	}
+	public String getPersonEmail() {
+		return personEmail;
+	}
+	public void setPersonEmail(String personEmail) {
+		this.personEmail = personEmail;
+	}
+	
+}
